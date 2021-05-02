a8f90a4be848ff41ecced240a2b7ad33ba2e4f0c
tim robillard <hello@timrobillard.ca>
2021-05-02 14:06:58 -0400

    add favicon and card resources

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -2,4 +2,4 @@ export const __prod__ = process.env.NODE_ENV === 'production';
 // export const __prod__ = true;
 export const SOCKET_URL = __prod__
     ? 'https://pass-the-ace.herokuapp.com/'
-    : 'http://localhost:5000/';
+    : 'http://192.168.0.11:5000/';
